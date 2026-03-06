13114282a6f655481eb85d97c1ef748486411a1c
jesusgomjez777 <gomezmoralesjesusalexander958@gmail.com>
2026-03-06 00:14:54 +0000

    wip

--- tienda/urls.py
+++ tienda/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.home, name ="home"), 
     path("productos/", views.lista_productos, name="lista_productos"),
     path("productos/<int:pk>/", views.detalle_producto, name="detalle_producto"),
+    path("producto/", views.detalle_producto, name="detalle_producto"),
 ]
