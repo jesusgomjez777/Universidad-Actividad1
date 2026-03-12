98ae5620c8c6f30ecc8a4fd485e857998a3eeb80
jesusgomjez777 <gomezmoralesjesusalexander958@gmail.com>
2026-03-12 04:49:20 +0000

    “mensaje”

--- tienda/urls.py
+++ tienda/urls.py
@@ -6,6 +6,9 @@ app_name = "tienda"
 urlpatterns = [
     path("", views.home, name="home"),
     path("programas/", views.lista_programa, name="lista_programa"),
+    path("programas/<str:pk>/", views.detalle_programa, name="detalle_programa"),
     path("cursos/", views.lista_curso, name="lista_curso"),
+    path("cursos/<str:pk>/", views.detalle_curso, name="detalle_curso"),
     path("estudiantes/", views.lista_estudiante, name="lista_estudiante"),
+    path("estudiantes/<str:pk>/", views.detalle_estudiante, name="detalle_estudiante"),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Programa, Curso, Estudiante
+from django.shortcuts import render, get_object_or_404
 
 def home(request):
     return render(request, "tienda/home.html", {})
+
+def detalle_programa(request, pk):
+    programa = get_object_or_404(Programa, pk=pk)
+    contexto = {"programa": programa}
+    return render(request, "tienda/detalle_programa.html", contexto)
 
 def lista_programa(request):
     """Vista para listar todos los programas académicos"""
@@ -11,11 +17,21 @@ def lista_programa(request):
     contexto = {'programas': programas}
     return render(request, 'tienda/lista_programa.html', contexto)
 
+def detalle_curso(request, pk):
+    curso = get_object_or_404(Curso, pk=pk)
+    contexto = {"curso": curso}
+    return render(request, "tienda/detalle_curso.html", contexto)
+
 def lista_curso(request):
     """Vista para listar todos los cursos"""
     cursos = Curso.objects.all()
     contexto = {'cursos': cursos}
     return render(request, 'tienda/lista_curso.html', contexto)
+
+def detalle_estudiante(request, pk):
+    estudiante = get_object_or_404(Estudiante, pk=pk)
+    contexto = {"estudiante": estudiante}
+    return render(request, "tienda/detalle_estudiante.html", contexto)
 
 def lista_estudiante(request):
     """Vista para listar todos los estudiantes"""
